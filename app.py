2c862516541eb00d98f824de07a577b31498cfb3
errjak <dioadyaw@gmail.com>
2022-11-22 02:50:06 +0700

    Baru upload

--- app.py
+++ app.py
@@ -4,21 +4,25 @@ import numpy as np
 from flask import Flask
 import os
 app = Flask(__name__)
-# model = pickle.load(open("model.pkl", "rb"))
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
-        sepal_length = float(request.form['sepal_length'])
-        sepal_width = float(request.form['sepal_width'])
-        petal_length = float(request.form['petal_length'])
-        petal_width= float(request.form['petal_width'])
+        PMu = float(request.form['PM2.5'])
+        PMs = float(request.form['PM10'])
+        No = float(request.form['NO'])
+        N02 = float(request.form['NO2'])
+        NOx = float(request.form['NOx'])
+        Nh3 = float(request.form['NH3'])
+        AqI = float(request.form['AQI'])
 
-        val = np.array([sepal_length, sepal_width, petal_length, petal_width])
+
+        val = np.array([PMu, PMs, No, N02, NOx, Nh3, AqI])
 
         final_features = [np.array(val)]
-        model_path = os.path.join('models', 'modelNB.pkl')
+        model_path = os.path.join('models', 'modelDelhi.pkl')
         model = pickle.load(open(model_path, 'rb'))
         res = model.predict(final_features)
 
